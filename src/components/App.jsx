5f8d5791037def2f38faf4224af531e66e5495f9
PawelPalasinski <pawel.palasinski@gmail.com>
2022-09-11 17:55:21 +0200

    not found

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,17 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
 import HomePage from './HomePage/HomePage';
 import MovieDetailsPage from './MovieDetailsPage/MovieDetailsPage';
+import NotFound from './NotFound/NotFound';
 
 export const App = () => {
-
   return (
     <>
       <Navigation />
       <Routes>
-      <Route path="/" element={<HomePage />}></Route>
-      <Route path="/movies/:id" element={<MovieDetailsPage />}></Route>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/movies/:id" element={<MovieDetailsPage />}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
-      </>
+    </>
   );
 };
---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,3 @@
+const NotFound = () => <h2>Page not found error 404</h2>;
+
+export default NotFound;
